989309799fba80b86062f58478917c2ac22f92be
hanburger <cheesehanburger@163.com>
2024-01-10 11:22:59 +0800

    dasdsa

---
+++ SpingCloud/nacos/cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -0,0 +1,45 @@
+package cn.itcast.user.web;
+
+import cn.itcast.user.config.PartternProperties;
+import cn.itcast.user.pojo.User;
+import cn.itcast.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+//@RefreshScope
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // @Value("${pattern.dateformat}")
+    // private String dateformat;
+
+    @Autowired
+    private PartternProperties partternProperties;
+
+    @GetMapping("now")
+    public String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(partternProperties.getDataformat()));
+    }
+    /**
+     * 路径： /user/110
+     *
+     * @param id 用户id
+     * @return 用户
+     */
+    @GetMapping("/{id}")
+    public User queryById(@PathVariable("id") Long id) {
+        return userService.queryById(id);
+    }
+}
